4327b81d9789204569b54b7ab8f2895aede72dac
Truong Anh Vu <122198260+truonganhvu205@users.noreply.github.com>
2023-12-27 10:29:34 +0700

    Update models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -10,7 +10,6 @@ class Booking(models.Model):
    def __str__(self):
       return self.first_name + ' ' + self.last_name
 
-# Add code to create Menu model
 class Menu(models.Model):
    name = models.CharField(max_length=200)
    price = models.IntegerField()
